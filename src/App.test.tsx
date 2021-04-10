dcd9d878f2b756f28639c19c02af4cd40df8b4f3
Gear Chalermkried <chalermkried@gmail.com>
2021-04-11 01:32:00 +0700

    test: pre-push hook

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,9 +4,6 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
 
-  // eslint-disable-next-line no-console
-  console.error('boom');
-
   const linkElement = screen.getByText(/learn react/i);
 
   expect(linkElement).toBeInTheDocument();
