713c47f08c951104462343073f51b84c56f11f0f
Gear Chalermkried <chalermkried@gmail.com>
2021-04-11 01:30:49 +0700

    fix(husky): pre-push hook

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          onClick={() => hello()}
         >
           Learn React
         </a>
