23b1e11af0647e4b91a4f89122e51b5adfead33f
Gear Chalermkried <chalermkried@gmail.com>
2021-04-11 18:16:22 +0700

    chore: adjust ignore files

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,19 +1,16 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-const CONSOLE_FAIL_TYPES = ['error', 'warn']
+const CONSOLE_FAIL_TYPES = ['error', 'warn'];
 
 CONSOLE_FAIL_TYPES.forEach((type) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
   (console as any)[type] = (message: string) => {
     throw new Error(
-      `Failing due to console.${type} while running test!\n\n${message}`,
-    )
-  }
-})
+      `Failing due to console.${type} while running test!\n\n${message}`
+    );
+  };
+});
 
 beforeEach(() => {
-  expect.hasAssertions()
-})
+  expect.hasAssertions();
+});
